c0ee1740ecc1cec59b04309ce6c907a8244633a2
Rong-Li <lirong619@gmail.com>
2021-04-02 18:06:49 -0700

    some bug fix

--- imtm_dbintegration.py
+++ imtm_dbintegration.py
@@ -20,6 +20,16 @@ def main():
     tmdb_collection = db.tmdb_movies
     reviews = db.reviews
 
+    imdb_keys = ['release_year','genres','imdb_avgRating','imdb_numVotes','Directors','Writers','casts','runtimeMinutes']
+
+    #remove null valued key-value pairs to save space
+    for key in imdb_keys:
+        imdb_collection.update_many(
+    #         {key: None},
+            { '$or': [ { key: None }, { key: '' }, { key: [] } ] },
+            {'$unset': { key: ''}}   
+        )
+
     tmdb_keys = ['genres','vote_average','vote_count','poster_path','original_language','popularity','budget'
                  ,'revenue','status','tagline','homepage','belongs_to_collection','release_date'
                  ,'overview','originalTitle','runtimeMinutes','company_name','keywords_name','num_of_cast'
@@ -41,28 +51,31 @@ def main():
     tmdb_iterator = tmdb_collection.find({}, {'release_date': 0, 'primaryTitle': 0, 'originalTitle': 0, 'isAdult': 0})
 
     for tmovie in tmdb_iterator:
+        _id = tmovie['_id']
         if imdb_collection.find({'_id': _id}).count() == 0:
             break
-        _id = tmovie['_id']
         imovie = imdb_collection.find({'_id': _id})[0]
-        for key in tmovie.keys():
-            #always use the casts from tmdb for popularity feature that only tmdb has
-            if key == 'casts':
-                imdb_collection.update_one({'_id': _id}
-                                , {'$set': {key: tmovie[key]}})
-            elif key == 'Directors' and key in imovie:
-                imdb_collection.update_one({'_id': _id}
-                                , {'$set': { key: entity_res(imovie[key], [tmovie[key]]) }})
-            elif key == 'genres' and key in imovie:
-                imdb_collection.update_one({'_id': _id}
-                                , {'$set': { key: entity_res(imovie[key], tmovie[key]) }})  
-            elif key == 'Writers' and key in imovie:
-                imdb_collection.update_one({'_id': _id}
-                                , {'$set': { key: entity_res(imovie[key], tmovie[key]) }})  
-            else:
-                imdb_collection.update_one( 
-                    {'_id': _id, key:{ '$exists': 0 }}
-                    , {'$set': {key: tmovie[key]}} )
+        try:
+            for key in tmovie.keys():
+                #always use the casts from tmdb for popularity feature that only tmdb has
+                if key == 'casts':
+                    imdb_collection.update_one({'_id': _id}
+                                    , {'$set': {key: tmovie[key]}})
+                elif key == 'Directors' and key in imovie:
+                    imdb_collection.update_one({'_id': _id}
+                                    , {'$set': { key: entity_res(imovie[key], [tmovie[key]]) }})
+                elif key == 'genres' and key in imovie:
+                    imdb_collection.update_one({'_id': _id}
+                                    , {'$set': { key: entity_res(imovie[key], tmovie[key]) }})  
+                elif key == 'Writers' and key in imovie:
+                    imdb_collection.update_one({'_id': _id}
+                                    , {'$set': { key: entity_res(imovie[key], tmovie[key]) }})  
+                else:
+                    imdb_collection.update_one( 
+                        {'_id': _id, key:{ '$exists': 0 }}
+                        , {'$set': {key: tmovie[key]}} )
+        except:
+            print(_id)
 
     #drop documents that does not have a review
     reviews.delete_many({'review': []})
