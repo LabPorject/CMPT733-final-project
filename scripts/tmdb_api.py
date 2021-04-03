9fb9e46528b1d3eddcd492ea8ab233de3b79c07c
Xu Zhicheng <maxx@sfu.ca>
2021-04-02 17:20:01 -0700

    my branch cleaning

--- scripts/tmdb_api.py
+++ scripts/tmdb_api.py
@@ -52,18 +52,13 @@ FIND() will only return very basic information
 def parseFindBasic(responses, imdb_id):
     fdict = {}
     fdict['_id'] = imdb_id
-    attr_list = ['id', 'title', 'vote_average', 'vote_count', 'poster_path', \
-                'genre_ids', 'original_language', 'popularity', 'release_date','original_title']
-
+    attr_list = ['id','genre_ids']
     for a in attr_list:
         fdict[a] =  responses['movie_results'][0][a]
+    fdict['genre_ids'] = list(map(genre_map, fdict['genre_ids']))
     fdict['tmdb_id'] = fdict.pop('id')
-    fdict['primaryTitle'] = fdict.pop('title')
-    fdict['originalTitle'] = fdict.pop('original_title')
-
-    fdict['genres'] = list(map(genre_map, fdict['genre_ids']))
-    fdict['poster_path'] = POSTER_PREFIX + fdict['poster_path'][1:]
-    fdict['overview'] =  responses['movie_results'][0]['overview']
+    fdict['genres'] = fdict.pop('genre_ids')
+    # fdict['overview'] =  responses['movie_results'][0]['overview']
     return fdict
 
 """
@@ -80,18 +75,25 @@ Parse the basic inforation with MOVIE() API call
 MOVIE() will only return more detailed information
 """
 def parseMovies(movie):
-    attr_list = ['budget', 'revenue', 'runtime', 'status', 'tagline', \
-                'adult','homepage','belongs_to_collection']
+    attr_list = ['title', 'vote_average', 'vote_count', 'poster_path', \
+                'original_language', 'popularity','original_title', \
+                'budget', 'revenue', 'runtime', 'status', 'tagline', \
+                'adult','homepage','belongs_to_collection','release_date','overview']
     mdict = {}
     movie = movie.info()
     for a in attr_list:
         mdict[a] = movie[a]
+
+    mdict['primaryTitle'] = mdict.pop('title')
+    mdict['originalTitle'] = mdict.pop('original_title')
     mdict['runtimeMinutes'] = mdict.pop('runtime')
+    if mdict['poster_path'] is not None:
+        mdict['poster_path'] = POSTER_PREFIX + mdict['poster_path'][1:]
     mdict['isAdult'] = mdict.pop('adult')
 
     if (movie['belongs_to_collection']) != None:
         mdict['belongs_to_collection'] = movie['belongs_to_collection']['name']
-    mdict['company_id'] = [i['id'] for i in movie['production_companies']]
+    # mdict['company_id'] = [i['id'] for i in movie['production_companies']]
     mdict['company_name'] = [i['name'] for i in movie['production_companies']]
     # print(mdict)
     return mdict
@@ -102,9 +104,11 @@ Used functions like keywords, credit, to get more information
 def parseMoreInfo(movie):
     midict = {}
     keywords = movie.keywords()
-    midict['keywords_id'] = [i['id'] for i in keywords['keywords']]
+    # midict['keywords_id'] = [i['id'] for i in keywords['keywords']]
     midict['keywords_name'] = [i['name'] for i in keywords['keywords']]
     cast = movie.credits()['cast']
+    if len(cast) == 0:
+        return midict
     df = pd.DataFrame(cast)
     midict['num_of_cast'] = len(df.index)
     top_10_cast = df.sort_values(['popularity'], ascending=False).head(10)
@@ -113,6 +117,8 @@ def parseMoreInfo(movie):
     midict['top_10_cast_popularity'] = top_10_cast['popularity'].tolist()
 
     crew = movie.credits()['crew']
+    if len(crew) == 0:
+        return midict
     df = pd.DataFrame(crew)
     df = df.drop_duplicates(subset=['name'])
     midict['num_of_crew'] = len(df.index)
@@ -134,13 +140,13 @@ Get all the user reviews with movie id as an input
 """
 def getReviews(movie, tmdb, imdb):
     reviews = movie.reviews()
-    all_review = {'id': tmdb,'imdb_id': imdb}
+    all_review = {'_id': imdb,'tmdb_id': tmdb}
     mre = []
     for r in reviews['results']:
         rdict = {}
         rdict['author'] = r['author']
         rdict['rating'] = r['author_details']['rating']
-        rdict['content'] = r['content']
+        # rdict['content'] = r['content']
         rdict['url'] = r['url']
         mre.append(rdict)
     all_review['review'] = mre
@@ -154,7 +160,8 @@ getInfoIMDB(imdb_id) will return basic movie information and movie reviews
 """
 if __name__ == '__main__':
     # imdb_id = 'tt5491994'
-    imdb_id = 'tt2850386'
+    imdb_id = 'tt0468569'
+
     results = getInfoIMDB(imdb_id)
     if results != None:
         minfo, mreview = results
