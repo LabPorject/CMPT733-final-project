b9b44221e8f15ef477d6cb43e51b78bb7de08ffb
Xu Zhicheng <maxx@sfu.ca>
2021-04-07 01:57:33 -0700

    tpot

--- scripts/tpot_auto.py
+++ scripts/tpot_auto.py
@@ -9,7 +9,7 @@ df = df.rename(columns={'imdb_avgRating': 'target'})
 X,y = df.drop(['target'],axis=1), df['target']
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-tpot = TPOTRegressor()
+tpot = TPOTRegressor(generations=5, population_size=5, verbosity=2, random_state=42)
 tpot.fit(X_train, y_train)
 print(tpot.score(X_test, y_test))
 tpot.export('tpot_pipeline.py')
