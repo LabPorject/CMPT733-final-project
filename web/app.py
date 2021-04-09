bd3322d5e23e383f6615323249cab7c6a2240948
Xu Zhicheng <maxx@sfu.ca>
2021-04-09 15:36:19 -0700

    web with random list

---
+++ web/app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def mainPage():
+    return render_template('home.html')
+
+@app.route('/rating')
+def predModelPage():
+    return render_template('pred_model.html')
+
+@app.route('/customize')
+def diyRatingPage():
+    return render_template('random_rating.html')
+
+@app.route('/<rdn>')
+def randomRatingPage(rdn):
+    if rdn == 'rdnrating':
+        return render_template('random_rating.html')
+    elif rdn == 'goodlist':
+        return render_template('random_rating.html')
+    else:
+        return """ <h1>Route Not Found</h1> """
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000,debug=True)
