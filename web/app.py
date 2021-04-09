f3d5b645771dc86b0fdad2b6ba4e782f11d3c028
Xu Zhicheng <maxx@sfu.ca>
2021-04-09 16:14:57 -0700

    app.py update

--- web/app.py
+++ web/app.py
@@ -1,9 +1,27 @@
-from flask import Flask
-import query_db_web as function
-
+from flask import Flask,render_template
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    columns = function.get_all_columns_name()
-    return str(columns)
+def mainPage():
+    return render_template('home.html')
+
+@app.route('/rating')
+def predModelPage():
+    return render_template('pred_model.html')
+
+@app.route('/customize')
+def diyRatingPage():
+    return render_template('random_rating.html')
+
+@app.route('/<rdn>')
+def randomRatingPage(rdn):
+    if rdn == 'rdnrating':
+        return render_template('random_rating.html')
+    elif rdn == 'goodlist':
+        return render_template('random_rating.html')
+    else:
+        return """ <h1>Route Not Found</h1> """
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000,debug=True)
