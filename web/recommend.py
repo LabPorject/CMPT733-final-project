4b7225122b95f89cd2cae15364547166074aad91
Chao <nickzhang0608@gmail.com>
2021-04-10 21:16:43 -0700

    recommend movie by id

---
+++ web/recommend.py
@@ -0,0 +1,31 @@
+import pickle
+import numpy as np
+import pandas as pd
+import os
+import sys
+module_path = os.path.abspath(os.path.join('../scripts'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+from similarity import SimilarityPredictions
+
+def get_recs_with_model(movie_id, sim_model):
+    #get similar movies
+    df = pd.read_csv('../all_movies_poster.csv')
+    output = sim_model.predict_similar_items(seed_item=movie_id, n=10)
+    similar_movies = pd.DataFrame(output)
+    similar_movies.set_index('item_id', inplace=True)
+    sim_df = pd.merge(df, similar_movies, left_index=True, right_index=True)
+    sim_df.sort_values('similarity_score', ascending=False, inplace=True)
+    return sim_df
+
+def get_recs(movie_id, embeddings):
+    #get similar movies
+    df = pd.read_csv('../all_movies_poster.csv')
+    sim_model = SimilarityPredictions(embeddings, similarity_metric="cosine")
+    output = sim_model.predict_similar_items(seed_item=movie_id, n=10)
+    similar_movies = pd.DataFrame(output)
+    similar_movies.set_index('item_id', inplace=True)
+    sim_df = pd.merge(df, similar_movies, left_index=True, right_index=True)
+    sim_df.sort_values('similarity_score', ascending=False, inplace=True)
+    return sim_df
+    # return sim_df.to_html()
