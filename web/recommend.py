c93f44a73735c7cb8f32bb96de445a415b6c77bb
Chao <nickzhang0608@gmail.com>
2021-04-11 18:34:53 -0700

    add hybrid recommendation

--- web/recommend.py
+++ web/recommend.py
@@ -47,3 +47,27 @@ def get_ensemble_recs_with_model(movie_id, model_1, model_2):
     sim_df_avg.drop("item_id", axis=1, inplace=True)
     sim_df_avg.reset_index(drop=True, inplace=True)
     return sim_df_avg.head(21).iloc[1:]
+
+def get_hybrid_recs_with_model(movie_id, content_model, collab_model):
+    #get similar movies from content 1
+    cont_output = content_model.predict_similar_items(seed_item=movie_id, n=21639)
+    similar_movies = pd.DataFrame(cont_output)
+    similar_movies.set_index('item_id', inplace=True)
+    sim_df_cont = similar_movies.rename(index=str, columns={"similarity_score": "similarity_score_1"})
+    #get similar movies from content 2
+    coll_output = collab_model.predict_similar_items(seed_item=movie_id, n=21639)
+    similar_movies = pd.DataFrame(coll_output)
+    similar_movies.set_index('item_id', inplace=True)
+    sim_df_coll = similar_movies.rename(index=str, columns={"similarity_score": "similarity_score_2"})
+    #ensemble results
+    sim_df_avg = pd.merge(sim_df_coll, pd.DataFrame(sim_df_cont['similarity_score_1']), left_index=True, right_index=True)
+    sim_df_avg['average_similarity_score'] = (sim_df_avg['similarity_score_1'] + sim_df_avg['similarity_score_1'] + sim_df_avg['similarity_score_2'])/3
+    sim_df_avg.drop("similarity_score_1", axis=1, inplace=True)
+    sim_df_avg.drop("similarity_score_2", axis=1, inplace=True)
+    sim_df_avg.reset_index(inplace=True)
+    sim_df_avg['item_id']=sim_df_avg['item_id'].astype(int)
+    sim_df_avg = df.merge(sim_df_avg, left_index=True, right_on='item_id')
+    sim_df_avg.sort_values('average_similarity_score', ascending=False, inplace=True)
+    sim_df_avg.drop("item_id", axis=1, inplace=True)
+    sim_df_avg.reset_index(drop=True, inplace=True)
+    return sim_df_avg.head(21).iloc[1:]
