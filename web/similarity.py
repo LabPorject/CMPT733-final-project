f62c6b795743b333e9828f5a75e7b9042b94ef6c
Chao <nickzhang0608@gmail.com>
2021-04-16 17:26:05 -0700

    remove euclidean distance

--- web/similarity.py
+++ web/similarity.py
@@ -10,26 +10,16 @@ class SimilarityPredictions(object):
             similarity_metric = str definining the similarity metrics to use'''
 
     def __init__(self, embeddings, similarity_metric='cosine'):
-        assert similarity_metric in ['cosine', 'euclidean'], "unsupported similarity metric."
+        assert similarity_metric in ['cosine'], "unsupported similarity metric."
         self.embeddings = embeddings
         self.ids = embeddings.index.tolist()
         self.similarity_metric = similarity_metric
         if similarity_metric == 'cosine':
             self.similarity_matrix = self.calculate_cosine_similarity_matrix()
-        if similarity_metric == 'euclidean':
-            self.similarity_matrix = self.calculate_euclidean_distances_matrix()
 
     def calculate_cosine_similarity_matrix(self):
         '''Calculates a cosine similarity matrix from the embeddings'''
         similarity_matrix = pd.DataFrame(cosine_similarity(
-            X=self.embeddings),
-            index=self.ids)
-        similarity_matrix.columns = self.ids
-        return similarity_matrix
-
-    def calculate_euclidean_distances_matrix(self):
-        '''Calculates a cosine similarity matrix from the embeddings'''
-        similarity_matrix= pd.DataFrame(euclidean_distances(
             X=self.embeddings),
             index=self.ids)
         similarity_matrix.columns = self.ids
@@ -41,8 +31,6 @@ class SimilarityPredictions(object):
         similar_items.columns = ["similarity_score"]
         if self.similarity_metric == 'cosine':
             similar_items = similar_items.sort_values('similarity_score', ascending=False)
-        if self.similarity_metric == 'euclidean':
-            similar_items = similar_items.sort_values('similarity_score', ascending=True)
         similar_items = similar_items.head(n)
         similar_items.reset_index(inplace=True)
         similar_items = similar_items.rename(index=str, columns={"index": "item_id"})
